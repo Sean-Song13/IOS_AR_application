04e635b86357786c6085ec021f9f77979ab71f99
theo <theo>
2021-11-16 13:14:06 -0600

    text

---
+++ AR-Demo/ios/tempViewController.swift
@@ -0,0 +1,29 @@
+//
+//  tempViewController.swift
+//  AR-Demo
+//
+//  Created by Guohao Tong on 11/16/21.
+//
+
+import UIKit
+
+class tempViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
