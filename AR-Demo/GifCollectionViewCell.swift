38ccbf5adbc4558f90b08d2913ecd1f3953ecb5c
MarkLu <MarkLu>
2021-11-16 13:45:00 -0600

    888

--- AR-Demo/GifCollectionViewCell.swift
+++ AR-Demo/GifCollectionViewCell.swift
@@ -16,7 +16,7 @@ class GifCollectionViewCell: UICollectionViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        // lzy test111222333
+        // lzy test11122233
     }
     
     public func configure(with gifName: String){
