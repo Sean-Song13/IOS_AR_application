023f0664edc0bad9b689fcdc2d51e86d11d7b0c7
Chongtian <zct8555328@gmail.com>
2021-12-05 01:30:01 -0600

    SSD

---
+++ AR-Demo/CommentView.swift
@@ -0,0 +1,29 @@
+//
+//  CommentView.swift
+//  AR-Demo
+//
+//  Created by Chongtian Zhang on 12/5/21.
+//
+
+import UIKit
+
+class CommentView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- AR-Demo/momentVC.swift
+++ AR-Demo/momentVC.swift
@@ -6,12 +6,63 @@
 //
 
 import UIKit
-
-class momentVC: UIViewController {
+class theMomentCell:UICollectionViewCell,UITableViewDelegate,UITableViewDataSource    {
+    
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userText: UILabel!
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var totalLikes: UILabel!
+    @IBOutlet weak var commentTable: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell=tableView.dequeueReusableCell(withIdentifier: "comment",for: indexPath)
+        cell.textLabel?.text="comment"//load comment here
+        return cell
+    }
+    
+    func loadComment(){
+        commentTable.delegate=self
+        commentTable.dataSource=self
+        commentTable.reloadData()
+    }
+}
+class momentVC: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var theMomentTable: UICollectionView!
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell=collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! theMomentCell
+        cell.userName.text = "SampleUser12345"
+        cell.userText.text = "HelloWorld!"
+        cell.userImage.image = UIImage(named:"large")
+        cell.loadComment()
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let commentVC=storyboard?.instantiateViewController(withIdentifier:"commentVC") as! CommentView
+        navigationController?.pushViewController(commentVC, animated: true)
+    }
+    
+    func setupMoments(){
+        theMomentTable.dataSource=self
+        theMomentTable.reloadData()
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        theMomentTable.delegate=self
+        setupMoments()
         // Do any additional setup after loading the view.
     }
     
