ca446137cc2ea25d1fe0579511ddae5ed20ef96e
Chongtian <zct8555328@gmail.com>
2021-12-05 02:03:22 -0600

    xd

--- AR-Demo/CommentView.swift
+++ AR-Demo/CommentView.swift
@@ -8,7 +8,26 @@
 import UIKit
 
 class CommentView: UIViewController {
-
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userText: UILabel!
+    @IBOutlet weak var userImage: UIImageView!
+    
+    @IBAction func Like(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var newCommentText: UITextField!
+    
+    @IBAction func submitComment(_ sender: Any) {
+        let check=newCommentText.text?.replacingOccurrences(of: " ", with: "")
+        if check != ""{
+            let newComment=newCommentText.text
+            //send to server
+        }
+        navigationController?.popViewController(animated: true)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
