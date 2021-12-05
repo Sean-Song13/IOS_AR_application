5ad31a311d64ce0406c828f643e8a8254e9f6046
Chongtian <zct8555328@gmail.com>
2021-12-05 01:45:11 -0600

    gas

--- AR-Demo/newMomentVC.swift
+++ AR-Demo/newMomentVC.swift
@@ -7,7 +7,40 @@
 
 import UIKit
 
-class newMomentVC: UIViewController {
+class newMomentVC: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+    
+    @IBOutlet weak var newText: UITextField!
+    @IBOutlet weak var newImage: UIImageView!
+    @IBAction func pickImage(_ sender: Any) {
+        self.getImage(sourceType: .photoLibrary)
+    }
+    
+    @IBAction func sub_New_Moment(_ sender: Any) {
+        submitNew()
+    }
+    
+    func getImage(sourceType: UIImagePickerController.SourceType){
+        if UIImagePickerController.isSourceTypeAvailable(sourceType) {
+            let imagePickerController = UIImagePickerController()
+            imagePickerController.delegate = self
+            imagePickerController.allowsEditing = true
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        self.dismiss(animated: true)
+        guard let image=info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {return}
+        newImage.image=image
+        //image is an UIImage object
+    }
+    
+    func submitNew(){
+        navigationController?.popViewController(animated: true)
+        //text is newText.text(String)
+        //picture is newImage.image(UIImage)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
